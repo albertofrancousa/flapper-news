51de5e5f3818e5a6b40de0fcc6510ea66b197d70
Alberto Franco <albertofrancodev@gmail.com>
2016-08-21 00:18:09 -0400

    Enable upvoting simply by adding a click event.

--- app.js
+++ app.js
@@ -18,6 +18,10 @@ angular.module('flapperNews', [])
 				if(!$scope.title || $scope.title === '') { return; }
 				$scope.posts.push({title: $scope.title, upvotes: 0});
 				$scope.title = '';
+			};
+			
+			$scope.incrementUpvotes = function(post) {
+				post.upvotes += 1;
 			};			
 		}
 	])
